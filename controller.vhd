1fbdc54f58e092f214c93e06ef02661c16b90c68
Erik Schubert <erikschubert@live.com>
2011-09-15 09:58:37 +0200

    some fixes
    #fix pwm counter doesn't count
    #fix pwm counter doesn't reset
    #fix range of pwm_clk_cnt_sig and pwm_cnt_on_sig

--- controller.vhd
+++ controller.vhd
@@ -17,32 +17,34 @@ entity controller is
 end controller;
 
 architecture behave of controller is
-  signal pwm_cnt_on_sig  : integer range 0 to 2**input_bit_width - 1 := 4999;
-  signal pwm_clk_cnt_sig : integer range 0 to 2**input_bit_width - 1 := 0;
+  signal pwm_cnt_on_sig  : integer range 0 to 10000 := 4999;
+  signal pwm_clk_cnt_sig : integer range 0 to 10000 := 0;
   signal pwm_out_sig     : std_logic;
-  signal cs_sig : std_logic;            -- internal signal for chip select
+  signal cs_sig          : std_logic;   -- internal signal for chip select
 
 begin  -- behave
 
   pwm_generation : process (clk, reset)
   begin  -- process pwm_generation
     if reset = '1' then
-      pwm_out_sig     <= '0';
-      cs_sig <= '0';
+      pwm_out_sig     <= '1';
+      cs_sig          <= '0';
       pwm_clk_cnt_sig <= 0;
       pwm_cnt_on_sig  <= 4999;
     elsif rising_edge(clk) then
-      if pwm_clk_cnt_sig = 9999 then
+       -- increment pwm-counter
+      pwm_clk_cnt_sig <= pwm_clk_cnt_sig + 1;
+      
+      if pwm_clk_cnt_sig > 9990  then
         pwm_clk_cnt_sig <= 0;
         pwm_out_sig     <= '1';
-      elsif pwm_clk_cnt_sig = pwm_cnt_on_sig then
-        pwm_clk_cnt_sig <= pwm_clk_cnt_sig + 1;
-        pwm_out_sig     <= '0';
+      elsif pwm_clk_cnt_sig > pwm_cnt_on_sig then
+        pwm_out_sig <= '0';
       end if;
     end if;
   end process pwm_generation;
 
   pwm_out <= pwm_out_sig;
-  cs <= not cs_sig;
+  cs      <= not cs_sig;
   
 end behave;
